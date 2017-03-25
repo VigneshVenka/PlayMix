0e577b611536c2a27f0ef36ccd4da11386e19872
Karthik Iyer <thekarthikiyer@outlook.com>
2017-03-25 13:18:40 -0400

    Initial commit "Hello, World!" textbox

---
+++ app/src/main/java/ritmhacks9/playmix/MainActivity.java
@@ -0,0 +1,13 @@
+package ritmhacks9.playmix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
